79a28effc320448e2d081b532b9c6a35d7474eb9
lai2 <informatica@lai2>
2019-12-03 14:28:56 +0100

    creazione giocatore2

--- BattagliaNavale/ServerBattagliaNavale/src/serverbattaglianavale/Giocatore.java
+++ BattagliaNavale/ServerBattagliaNavale/src/serverbattaglianavale/Giocatore.java
@@ -6,6 +6,7 @@
 package serverbattaglianavale;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -16,6 +17,7 @@ import java.util.Scanner;
  */
 public class Giocatore implements Runnable{
     Scanner input;
+    PrintWriter output;
     Socket socket;
     private ArrayList<Barca> barche;
 
@@ -33,6 +35,7 @@ public class Giocatore implements Runnable{
     
     private void setup() throws IOException {
         input = new Scanner(socket.getInputStream());
+        output = new PrintWriter(socket.getOutputStream());
     }
     
 }
