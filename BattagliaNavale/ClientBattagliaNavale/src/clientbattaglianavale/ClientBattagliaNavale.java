c770acd71a2be41a553b6ecb14d1dd959bc668c4
Andrea Cei <andreacei21@gmail.com>
2019-12-18 19:26:46 +0100

    completato controllo inserimento barche

--- BattagliaNavale/ClientBattagliaNavale/src/clientbattaglianavale/ClientBattagliaNavale.java
+++ BattagliaNavale/ClientBattagliaNavale/src/clientbattaglianavale/ClientBattagliaNavale.java
@@ -9,11 +9,13 @@ public class ClientBattagliaNavale {
     
     Socket socket;
     Scanner in;
+    Scanner tastiera;
     PrintWriter out;
     
     public ClientBattagliaNavale () throws IOException  {
         
         socket = new Socket("127.0.0.1", 42069);
+        tastiera = new Scanner(System.in);
         in = new Scanner(socket.getInputStream());
         out = new PrintWriter(socket.getOutputStream(), true);
 }
@@ -23,11 +25,37 @@ public class ClientBattagliaNavale {
      
         if (in.hasNextLine()) {
             String testo = in.nextLine();
-            if (testo) {
-                
-            }
+            if (testo.startsWith("INS"))
+                inserimentoBarca(testo.charAt(4));
         }
     }  
+    
+    private void inserimentoBarca(char ins) {
+        if (ins == 'S') {
+            System.out.println("Inserire coordinate e orientamento barca dell'inizio della barca(se orizzontale si estende a destra e in verticale verso il basso)(le coordinate vaanno scritte in lettere es: 1 = a, 2 = b, etc..) es. barca gto (x = g = 7, y = t = 20, o = orizzontale)");
+   
+        }
+        if (ins == 'E') {
+            System.out.println("Errore nel valore inserito");
+            this.inserimentoBarca('S');
+        }
+        if (ins == '5') {
+            System.out.println("Inserire barca da 5 caselle");
+            out.println(tastiera.nextLine());
+        }
+        if (ins == '4') {
+            System.out.println("Inserire barca da 4 caselle");
+            out.println(tastiera.nextLine());
+        }
+        if (ins == '3') {
+            System.out.println("Inserire barca da 3 caselle");
+            out.println(tastiera.nextLine());
+        }
+        if (ins == '2') {
+            System.out.println("Inserire barca da 2 caselle");
+            out.println(tastiera.nextLine());
+        }
+    } 
     
     private void gioca() {
         this.interpreta();
--- BattagliaNavale/ServerBattagliaNavale/src/serverbattaglianavale/Gioco.java
+++ BattagliaNavale/ServerBattagliaNavale/src/serverbattaglianavale/Gioco.java
@@ -52,7 +52,130 @@ public class Gioco {
             
         }
     }
-    //AGGIUNGERE CONTROLLO DEI CONFLITTI TRA LE BARCHE
+    
+    private boolean controlloIns(Barca barca) {
+        if (barche.isEmpty())
+            return true; //true equivale a inserimento corretto
+        int x = barca.getInizio().getX();
+        int y = barca.getInizio().getY();
+                
+        if (x == 0 && y == 0) { //angolo Alto-Sinistra
+            if (griglia[x][y] == 1
+                || griglia[x + 1][y + 1] == 1
+                || griglia[x + 1][y] == 1
+                || griglia[x][y + 1] == 1)
+                return false;
+            if (barca.getOrientamento() == 'o')
+                x++;
+            else
+                y++;
+        }
+            
+        
+        if (x == 20 && y == 0) { //angolo Alto-Destra
+            if (griglia[x][y] == 1
+                || griglia[x - 1][y] == 1
+                || griglia[x - 1][y + 1] == 1
+                || griglia[x][y + 1] == 1)
+                return false;
+            y++;
+        }
+        
+        if (x == 0 && y == 20) { //angolo Basso-Sinistra
+            if (griglia[x][y] == 1
+                || griglia[x][y - 1] == 1
+                || griglia[x + 1][y - 1] == 1
+                || griglia[x + 1][y] == 1)
+                return false;
+            x++;
+        }
+        
+        
+        if (x == barca.getInizio().getX() && y == barca.getInizio().getY()) { //controllo inizio generico
+            if (griglia[x][y] == 1 
+                || griglia[x + 1][y + 1] == 1
+                || griglia[x - 1][y - 1] == 1
+                || griglia[x + 1][y] == 1
+                || griglia[x][y + 1] == 1
+                || griglia[x - 1][y] == 1
+                || griglia[x][y - 1] == 1)
+                return false;
+            if (barca.getOrientamento() == 'o')
+                x++;
+            else
+                y++;
+        }
+                    
+        if (barca.getOrientamento() == 'o') { //ciclo per barche orizzontali
+            while ( x <= barca.getFine().getX()) {
+                if (x == 20 && y == 20) { //angolo Basso-Destra
+                    if (griglia[x][y] == 1
+                        || griglia[x -1][y] == 1
+                        || griglia[x - 1][y - 1] == 1
+                        || griglia[x][y - 1] == 1)
+                        return false;
+                }
+                if (x == 20 && y == 0) { //angolo Alto-Destra
+                    if (griglia[x][y] == 1
+                        || griglia[x - 1][y] == 1
+                        || griglia[x - 1][y + 1] == 1
+                        || griglia[x][y + 1] == 1)
+                        return false;
+                }
+                if (x == 0 && y == 20) { //angolo Basso-Sinistra
+                    if (griglia[x][y] == 1
+                        || griglia[x][y - 1] == 1
+                        || griglia[x + 1][y - 1] == 1
+                        || griglia[x + 1][y] == 1)
+                        return false;
+                }
+                if (griglia[x][y] == 1
+                    || griglia[x + 1][y + 1] == 1
+                    || griglia[x - 1][y - 1] == 1
+                    || griglia[x + 1][y] == 1
+                    || griglia[x][y + 1] == 1
+                    || griglia[x - 1][y] == 1
+                    || griglia[x][y - 1] == 1)
+                    return false;
+                x++;
+            }
+        } else
+            while ( y <= barca.getFine().getY()) { //ciclo per barche verticali
+                if (x == 20 && y == 20) { //angolo Basso-Destra
+                    if (griglia[x][y] == 1
+                        || griglia[x -1][y] == 1
+                        || griglia[x - 1][y - 1] == 1
+                        || griglia[x][y - 1] == 1)
+                        return false;
+                }
+                if (x == 20 && y == 0) { //angolo Alto-Destra
+                    if (griglia[x][y] == 1
+                        || griglia[x - 1][y] == 1
+                        || griglia[x - 1][y + 1] == 1
+                        || griglia[x][y + 1] == 1)
+                        return false;
+                }
+                if (x == 0 && y == 20) { //angolo Basso-Sinistra
+                    if (griglia[x][y] == 1
+                        || griglia[x][y - 1] == 1
+                        || griglia[x + 1][y - 1] == 1
+                        || griglia[x + 1][y] == 1)
+                        return false;
+                }
+                if (griglia[x][y] == 1
+                    || griglia[x + 1][y + 1] == 1
+                    || griglia[x - 1][y - 1] == 1
+                    || griglia[x + 1][y] == 1
+                    || griglia[x][y + 1] == 1
+                    || griglia[x - 1][y] == 1
+                    || griglia[x][y - 1] == 1)
+                    return false;
+                y++;
+            }
+        return true;
+    }
+    
+    
     private Barca interpretaIns(String testo) {
         Barca b = new Barca();
         if(testo != null || testo.length() == 3) {
@@ -61,36 +184,55 @@ public class Gioco {
             int x = Character.getNumericValue(testo.charAt(0));
             int y = Character.getNumericValue(testo.charAt(1));
             char orient = testo.charAt(2);
-            if ( x >= 10 && x <= 30 ) //controllo che il valore di x sia una lettera dell'alfabeto
-                if ( y >= 10 && y <= 30 ) //controllo che il valore di y sia una lettera dell'alfabeto
+            if ( x >= 10 && x <= 30 ){ //controllo che il valore di x sia una lettera dell'alfabeto. a = 0 -> u = 21
+                if ( y >= 10 && y <= 30 ) //controllo che il valore di y sia una lettera dell'alfabeto. a = 0 -> u = 21
                     if (orient == 'o' || orient == 'v') //controllo che orient sia o (orizzontale) oopure v (verticale)
                     {
-                        b.setInizio(new Coordinata(Character.getNumericValue(x) - 9, Character.getNumericValue(y) - 9));
+                        b.setInizio(new Coordinata(Character.getNumericValue(x) - 10, Character.getNumericValue(y) - 10));
                         b.setOrientamento(orient);
                         if (orient == 'o') //calcolo la coordinata di fine della barca
-                            b.setFine(new Coordinata(Character.getNumericValue(x) - 9 + b.getLunghezza(), Character.getNumericValue(y) - 9));
+                            b.setFine(new Coordinata(Character.getNumericValue(x) - 10 + b.getLunghezza(), Character.getNumericValue(y) - 10));
                         else
-                            b.setFine(new Coordinata(Character.getNumericValue(x) - 9, Character.getNumericValue(y) - 9 + b.getLunghezza()));
+                            b.setFine(new Coordinata(Character.getNumericValue(x) - 10, Character.getNumericValue(y) - 10 + b.getLunghezza()));
                     }
-            else
-                     output.println("Errore nell'inserimento dei dati della barca");
+                else
+                    output.println("INSE"); //prefisso INS sta per inserimento, E sta per "errore nell'inserimento"
+                else
+                    output.println("INSE"); //prefisso INS sta per inserimento, E sta per "errore nell'inserimento"
+            }   else
+                    output.println("INSE"); //prefisso INS sta per inserimento, E sta per "errore nell'inserimento"
         }
          return b;
     }
     
     private void inserisciBarche() {
+        Barca temp = new Barca();
         output.println("INSS"); //prefisso INS sta per inserimento, S sta per "inizio inserimento"
+        do {
         output.println("INS5"); //prefisso INS sta per inserimento, 5 sta per "inserisci barca da 5"
-        barche.add(interpretaIns(input.nextLine()));
+        temp = interpretaIns(input.nextLine());
+        } while(controlloIns(temp) == false); //Ripete finche il controllo non va a buon fine
+        
+        barche.add(temp);
+        do{
         output.println("INS4"); //prefisso INS sta per inserimento, 4 sta per "inserisci barca da 4"
-        barche.add(interpretaIns(input.nextLine()));
+        temp = interpretaIns(input.nextLine());
+        } while (controlloIns(temp) == false); //Ripete finche il controllo non va a buon fine
+        
+        barche.add(temp);
         for (int i = 0; i < 2; i++) { //ci sono 2 barche da 3
+            do {
             output.println("INS3"); //prefisso INS sta per inserimento, 3 sta per "inserisci barca da 3"
-            barche.add(interpretaIns(input.nextLine()));
+            temp = interpretaIns(input.nextLine());
+            } while(controlloIns(temp) == false); //Ripete finche il controllo non va a buon fine
+            barche.add(temp);
         }
         for (int i = 0; i < 3; i++) { //ci sono 3 barche da 2
+            do {
             output.println("INS2"); //prefisso INS sta per inserimento, 2 sta per "inserisci barca da 2"
-            barche.add(interpretaIns(input.nextLine()));
+            temp = interpretaIns(input.nextLine());
+            }while(controlloIns(temp) == false); //Ripete finche il controllo non va a buon fine
+            barche.add(temp);
         }
         
         
